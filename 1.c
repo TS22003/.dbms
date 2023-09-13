c0aa1382b3f63c9fc9befa5bf693704164d68939
VISHRUTH V S <88739731+VishruthVS@users.noreply.github.com>
2023-09-13 19:54:33 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,16 +1,3 @@
-/*****************************************************************************************************
-    Consider a structure named Student with attributes as SID, NAME, 
-    BRANCH, SEMESTER, ADDRESS. 
-    Write a program in C/C++/ and perform the following operations using 
-    the concept of files.
-    a. Insert a new student
-    b. Modify the address of the student based on SID
-    c. Delete a student
-    d. List all the students
-    e. List all the students of CSE branch.
-    f. List all the students of CSE branch and reside in Kuvempunagar.
-*****************************************************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
