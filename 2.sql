ff5b278f7f75eb4d6c7a4f93d5b685e87310d065
VISHRUTH V S <88739731+VishruthVS@users.noreply.github.com>
2023-12-27 07:44:33 +0530

    Update 2.sql

--- 2.sql
+++ 2.sql
@@ -1,8 +1,6 @@
 DROP DATABASE IF EXISTS student_104;
-
 CREATE DATABASE student_104;
 USE student_104;
-
 CREATE TABLE IF NOT EXISTS Student (
     sid VARCHAR(25) primary key,
     name VARCHAR(35) not null,
@@ -12,21 +10,14 @@ CREATE TABLE IF NOT EXISTS Student (
     phone VARCHAR(35),
     email VARCHAR(50)
 );
-
 INSERT INTO Student VALUES 
 ("1","A","CSE",5,"Bogadi","9432615389","anA@gmail.com"),
 ("2","B","EC",5,"Banglore","9776142539","beB@gmail.com"),
 ("3","C","CSE",5,"Kuvempunagar","9437465882","csCE@gmail.com"),
 ("4","D","EI",5,"Hebbal","93516264738","dh@gmail.com");
-
 UPDATE Student SET address="Vijaynagar" where sid ="2";
-
 SELECT * FROM Student;
-
 delete from Student where sid = "4";
-
 SELECT * FROM Student;
-
 SELECT * from Student where branch="CSE";
-
 SELECT * from Student where branch="CSE" and address="Kuvempunagar";
